4018e4947e4a5222a6f29187c11173fb5aea3d5d
Tim Felgentreff <tim.felgentreff@oracle.com>
2025-02-03 22:42:58 +0100

    include ham_convert

--- tools/png2gpl.py
+++ tools/png2gpl.py
@@ -9,5 +9,13 @@ with open(sys.argv[2], "w") as f:
     print(f"Name: '{sys.argv[1]}'", file=f)
     print("Columns: 16", file=f)
     print("#", file=f)
-    for idx, (_, (r, g, b)) in enumerate(i.getcolors()):
-        print(r, g, b, "    Index", idx, file=f)
+    if i.mode == "P":
+        for idx, (_, color) in enumerate(i.getcolors()):
+            if idx >= 32:
+                break
+            print(*i.getpalette()[color * 3:color * 3 + 3], "    Index", idx, file=f)
+    else:
+        for idx, (_, (r, g, b, *_)) in enumerate(i.getcolors()):
+            if idx >= 32:
+                break
+            print(r, g, b, "    Index", idx, file=f)
